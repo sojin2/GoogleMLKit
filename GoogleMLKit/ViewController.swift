6a7d353bdc44a12bfcdd22b54a87caf0788277ca
ssooo2164 <ssooo2164@gmail.com>
2023-08-22 11:31:07 +0900

    Setting

---
+++ GoogleMLKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoogleMLKit
+//
+//  Created by 김소진 on 2023/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
